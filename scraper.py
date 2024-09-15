01eacc6cb866175bda3b2d599ef7b21895af0e0f
lchristus <130999550+lchristus@users.noreply.github.com>
2024-09-15 09:44:35 -0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,2 +1,31 @@
+import os
+import requests
+import telebot
+from bs4 import BeautifulSoup
+import time
+
+gameids = []
+bot = telebot.Telebot(os.environ["TELEGRAM_TOKEN"])
+chatid = os.environ["NFLCHAT"]
+hosturl = "nfl-notification.onrender.com/status"
+espnurl = "espn.com/nfl/game/_/gameId/"
+scores = {}
+
 def start():
-  return 1
+  bot.sendmessage(chatid, "this still work")
+  return
+
+def scrape():
+  for id in ids:
+    s = requests.Session()
+    page = s.get(espnurl + id)
+    soup = BeautifulSoup(page, "html.parser")
+    scorecard = soup.find("div", "Gamestrip_Competitors relative flex Gamestrip_Competitors--boarder")
+    names = scorecard.findall("div", "ScoreCell_Teamname Scorecell_Teamname--abrev")
+    score = scorecard.findall("div", "Gamestrip_Score relative tc w-100 fw-heading-100 h3 clr-gray-01")
+    if score.size() != 2 or names.site() != 2:
+      continue
+    complete_score = score[0].text+':'+score[1].text
+    if scores[id] != complete_score:
+      bot.sendmessage(chatid, names[0].text+' '+complete_score+' '+names[1].text)
+      s.get(hosturl)
