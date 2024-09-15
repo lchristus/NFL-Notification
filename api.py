2dd27472a3a9a29d06e4666e16bd7fedd2e8461b
lchristus <130999550+lchristus@users.noreply.github.com>
2024-09-15 08:03:33 -0600

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+import scraper 
 
 app = FastAPI()
 
 @app.get("/status")
 def root():
     return {"status": "working"}
+
+@app.get("")
+def root():
+    scraper.start()
+    return {"status": "starting"}
